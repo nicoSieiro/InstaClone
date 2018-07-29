f3aebfe690c9fe8bd5d31bd5461725bcab7e2cac
nico <sieiro.nicolas@gmail.com>
2018-07-29 13:31:31 -0700

    fix last commit

--- App.js
+++ App.js
@@ -5,12 +5,7 @@ import * as firebase from 'firebase';
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDlVnYRX1Ojup91O6wUpUdiSqPjJzjHq6E",
-  authDomain: "instaclone-e087e.firebaseapp.com",
-  databaseURL: "https://instaclone-e087e.firebaseio.com",
-  projectId: "instaclone-e087e",
-  storageBucket: "instaclone-e087e.appspot.com",
-  messagingSenderId: "945356985971"
+  
 };
 firebase.initializeApp(config);
 
